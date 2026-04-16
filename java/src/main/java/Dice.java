b6839677eca808d76eae147724dbdd1cc09e7dbc
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-04-16 13:52:30 -0400

    My first virtual dice.

--- java/src/main/java/Dice.java
+++ java/src/main/java/Dice.java
@@ -1,4 +1,13 @@
+
+import java.util.Random;
+
+
 public class Dice {
 
+    public static int dice(){
+        Random rand = new Random();
+        int n = rand.nextInt(6 - 1 + 1)+1;
+        return n;
+    }
 
 }
--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -1,5 +1,8 @@
 public class Simulation {
 
+    public static void main(String[] args) {
+        System.out.println("First Dice: " + Dice.dice());
+    }
 
 
 }
